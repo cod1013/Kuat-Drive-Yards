48bdb8ebb315d9f1f28a7dd5c4fcbfa6690ac526
cod1013 <36607427+cod1013@users.noreply.github.com>
2018-02-18 14:19:04 -0600

    Create Script1.sh

---
+++ Script1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "************************"
+echo "* Please select and option:     *"
+echo "* 1. Create Group               *"
+echo "* Or any key to exit            *"
+echo "***********************"
+
+read menu_input
+
+case $menu_input in
+1)  echo "==== Group Creation ===="
+    echo -n "Please enter a group name: "
+    read group_name
+    
+    group_test=$(cat /etc/group | awk -F: '{ print $1 }" | grep $group_name)
+    
+    if [ -z $group_test ] || [ ! "$group_test" = "$group_name" ]; then
+        /usr/sbin/groupadd "$group_name"
+        mkdir /home/$group_name
+        chgrp $group_name /home/$group_name
+        chmod 0770 /home/$group_name
+        echo "Group and directory added"
+    else
+        echo "Group already exists!"
+    fi
+;;
+*) echo "EXIT"
+esac
