55071a9c7a36432c4e5fc119a0ba2e8d435452ac
Ecodner <36611810+Ecodner@users.noreply.github.com>
2018-02-20 01:47:21 +0000

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -11,13 +11,13 @@ read menu_input
 case $menu_input in
 1)  echo "==== Group Creation ===="
     echo -n "Please enter a group name: "
-    read group_neme
+    read group_name #changed group_neme to group_name
     
-    group_test=$(cat /etc/group | awk -F: '{ print $1 }" | grep $group_name)
+    group_test=$(cat /etc/group | awk -F: '{ print $1 }' | grep $group_name) #changed '{print $1}" to '{print $1}'
     
     if [ -z $group_test ] || [ ! $group_test" = "$group_name" ]; then
         /usr/sbin/groupadd "$group_name"
-        mkdir /home/$grup_name
+        mkdir /home/$group_name #Typo with Group name
         chgrp $group_name /home/$group_name
         chmod 0770 /home/$group_name
         echo "Group and directory added"
